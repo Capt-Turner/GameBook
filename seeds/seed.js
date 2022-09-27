4bb24ac0835359b6c39af2d425c714ccbac0fc59
Seth Turner <sethhet@gmail.com>
2022-09-26 20:41:19 -0600

    starting draft models

---
+++ seeds/seed.js
@@ -0,0 +1,16 @@
+const sequelize = require('../config/connection');
+const { Game } = require('../models');
+
+const gameData = require('../gameData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({force:true});
+    await Game.bulkCreate(gameData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    process.exit(0);
+};
+
+seedDatabase();
