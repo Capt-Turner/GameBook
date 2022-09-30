68f3a214954be338e320e1d9d03a810a7899164a
Nathan Aramyan <nathan.aramyan@gmail.com>
2022-09-29 19:48:31 -0600

    api update

--- controllers/tempAPI.js
+++ controllers/tempAPI.js
@@ -1,24 +1,23 @@
 import { response } from "express";
-import fetch from "node-fetch";
 
 // function getAPI(){
 //     fetch(`https://api.igdb.com/v4`)
 // }
- const clientID = "zdya2d3e1t6iad3gj9jkwjjg5fckfq";
- const clientSecret = "gwi6r93fp8oeals6n6f55khs6rxa8a";
+//  const clientID = "zdya2d3e1t6iad3gj9jkwjjg5fckfq";
+//  const clientSecret = "gwi6r93fp8oeals6n6f55khs6rxa8a";
 
-function getToken(clientID, clientSecret){
-    fetch(`https://id.twitch.tv/oauth2/token?client_id=${clientID}&client_secret=${clientSecret}&grant_type=client_credentials`)
-    .then(response => response.json())
-    .then(response => {
-        console.log(response);
-    })
-}
-getToken(clientID, clientSecret);
+// function getToken(clientID, clientSecret){
+//     fetch(`https://id.twitch.tv/oauth2/token?client_id=${clientID}&client_secret=${clientSecret}&grant_type=client_credentials`)
+//     .then(response => response.json())
+//     .then(response => {
+//         console.log(response);
+//     })
+// }
+// getToken(clientID, clientSecret);
 
-module.exports = getToken;
+// module.exports = getToken;
 
-console.log("hello");
+// console.log("hello");
 
 // async function pullToken (url = '') {
 //     const response = await fetch(`https://id.twitch.tv/oauth2/token?client_id=${clientID}&client_secret=${clientSecret}&grant_type=client_credentials`)
@@ -32,3 +31,22 @@ console.log("hello");
 // .then((data) => {
 // console.log(data)
 // });
+
+import axios from "axios";
+
+const options = {
+  method: 'POST',
+  url: 'https://api.igdb.com/v4/games',
+  headers: {
+    'Client-ID': 'zdya2d3e1t6iad3gj9jkwjjg5fckfq',
+    Authorization: 'bearer 4gilqa55x0teggl168rgf1nbsk3hck',
+    'Content-Type': 'text/plain'
+  },
+  data: 'fields *;\nwhere id = 85450;'
+};
+
+axios.request(options).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
