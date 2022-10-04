44c9fce27badae83291e1df2f577eca8b54aa1e8
Nathan Aramyan <nathan.aramyan@gmail.com>
2022-10-03 20:49:01 -0600

    Login Handlebar

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,5 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-=======
 const axios = require('axios').default;
->>>>>>> 05bf73b8b2591a780b25c838e5aa89812916181a
 const userRoutes = require('./userRoutes');
 
 router.use('/users', userRoutes);
