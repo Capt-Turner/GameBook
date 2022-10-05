631b123b2acae01a96b0d87d9db4ba43e61e5d14
Seth Turner <sethhet@gmail.com>
2022-10-04 20:50:13 -0600

    Very basic allmost MVP

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,30 +5,30 @@ const withAuth = require('../utils/auth');
 const sequelize = require('../config/connection');
 
 router.get('/', withAuth, async (req, res) => {
-    try {
-      const userData = await User.findAll({
-        attributes: { exclude: ['password'] },
-        order: [['name', 'ASC']],
-      });
-  
-      const users = userData.map((project) => project.get({ plain: true }));
-  
-      res.redirect('/homepage', {
-        users,
-        logged_in: req.session.logged_in,
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['name', 'ASC']],
+    });
+
+    const users = userData.map((project) => project.get({ plain: true }));
+
+    res.redirect('/', {
+      users,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-  
+
 router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('login');
+  if (req.session.logged_in) {
+    res.redirect('/homepage');
+    return;
+  }
+
+  res.render('login');
 });
 
 router.get('/homepage', async (req, res) => {
@@ -37,45 +37,36 @@ router.get('/homepage', async (req, res) => {
     url: "https://api.igdb.com/v4/games",
     method: 'POST',
     headers: {
-        'Accept': 'application/json',
-        'Client-ID': process.env.CLIENT_ID,
-        'Authorization': process.env.TOKEN,
+      'Accept': 'application/json',
+      'Client-ID': process.env.CLIENT_ID,
+      'Authorization': process.env.TOKEN,
     },
-    data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;"
+    data: "fields cover.image_id,genres,involved_companies,keywords,name,parent_game,platforms,release_dates.date,screenshots,summary,tags,url,websites;"
   })
     .then(response => {
       console.log(response.data);
       gameData = response.data;
-      const storeGames = async () => {
-        await sequelize.sync({force:true});
-        await Game.bulkCreate(gameData, {    
-        individualHooks: true,
-        returning: true,
-      });
-      storeGames();
-    };
       res.render('homepage', {
         gameData,
         loggedIn: req.session.loggedIn
-        });
+      });
     })
-    .catch(err => {
-        console.error(err);
-    });
-});
+        .catch(err => {
+          console.error(err);
+        });
+  })
+  router.get('/game/:id', withAuth, async (req, res) => {
+    try {
 
-router.get('/game/:id', withAuth, async (req, res) => {
-  try {
-    
-    const games = gameData.get({ plain: true });
-    res.render('browse', { games, loggedIn: req.session.loggedIn });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+      const games = gameData.get({ plain: true });
+      res.render('browse', { games, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
 
-router.get('/review', (req, res) => {
-  
-})
-module.exports = router;
+  router.get('/review', (req, res) => {
+
+  })
+  module.exports = router;
